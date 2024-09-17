95b68627ee0c7a9cc36c892d1bb18bb8b7a14cd5
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-09-17 15:48:42 -0300

    update users

--- src/App.java
+++ src/App.java
@@ -6,15 +6,17 @@ public class App {
         User u = new User();
         u.setEmail_user("luiz@email");
         u.setSenha_user("luiz");
+        new UserDao().cadastrarUser(u);
 
         User u2 = new User();
         u2.setEmail_user("pedro@email");
         u2.setSenha_user("pedro");
+        new UserDao().cadastrarUser(u2);
 
         User u3 = new User();
         u3.setEmail_user("guilherme@email");
         u3.setSenha_user("guilherme");
+        new UserDao().cadastrarUser(u3);
 
-        new UserDao().cadastrarUser(u);
     }
 }
