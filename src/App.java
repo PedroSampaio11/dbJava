adc21f195c08cad569c324b277dd88ff9ff7310c
PedroSampaioDev <pedrosampaio11@icloud.com>
2024-09-17 15:46:35 -0300

    add users

--- src/App.java
+++ src/App.java
@@ -4,8 +4,16 @@ import entity.User;
 public class App {
     public static void main(String[] args) throws Exception {
         User u = new User();
-        u.setEmail_user("PedroTeste@email.com");
-        u.setSenha_user("PedroTeste");
+        u.setEmail_user("luiz@email");
+        u.setSenha_user("luiz");
+
+        User u2 = new User();
+        u2.setEmail_user("pedro@email");
+        u2.setSenha_user("pedro");
+
+        User u3 = new User();
+        u3.setEmail_user("guilherme@email");
+        u3.setSenha_user("guilherme");
 
         new UserDao().cadastrarUser(u);
     }
